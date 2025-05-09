f5c874db6f399a434d7a57c41fd69c11de3d0050
Taaj Street <tstr0006@student.monash.edu>
2025-05-09 16:50:34 +1000

    added api usage examples

---
+++ examples/C_examples/manual.c
@@ -0,0 +1,44 @@
+#include "../nunavut_out/nova/motor_driver/Command_1_0.h"
+#include "nova_can.h"
+
+int motor_driver_command_callback(nova_motor_driver_Command_1_0 *command) {
+    switch (command->mode){
+        case nova_motor_driver_Command_1_0_CURRENT:
+            drive_current(command->value);
+        case nova_motor_driver_Command_1_0_VELOCITY:
+            drive_velocity(command->value);
+        case nova_motor_driver_Command_1_0_CURRENT:
+            drive_position(command->value);
+    }
+}
+
+int main() {
+
+    NovaCAN nova_can = nova_can_init(
+        1           //Node ID
+    );
+
+    nova_can_add_rx(&nova_can, //pointer to the nova_can struct
+                    50, //Subject ID
+                    motor_driver_command_callback, //the command callback
+    );
+
+
+    uint32_t mask = nova_can_generate_id_mask(&nova_can);
+    uint32_t filter = nova_can_generate_id_filter(&nova_can);
+
+    //hardware specific code to add mask and filter here.
+
+    while(true) {
+        // hardware specific code to get can messages
+        ...
+        // process the recieved message
+        nova_can_proccess_rx(&nova_can, //pointer to the nova_can struct
+                             *can_id,   //pointer to the can_id
+                             *data_buff,//pointer to the can data buffer
+                             data_length, // length of the data
+        )
+    }
+
+    return 1;
+}
