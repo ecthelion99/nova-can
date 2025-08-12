99931cf05dd1a17edbbe575652ef228317998a85
Matt van Wijk <mvanwijk12@gmail.com>
2025-08-12 15:23:03 +1000

    hardware in loop test success

--- tooling/mqtt_handler/can_mqtt_handler.py
+++ tooling/mqtt_handler/can_mqtt_handler.py
@@ -1,22 +1,24 @@
 import os
-import threading
 from nova_can.utils.compose_system import get_compose_result_from_env
 from nova_can.communication import CanReceiver, CanCallback
-import json
-
-import paho.mqtt.client as mqtt
+import time
+import random
+from paho.mqtt.enums import CallbackAPIVersion, MQTTErrorCode
+from paho.mqtt import client as mqtt_client
 
 # MQTT configuration (can be set via environment variables)
 MQTT_BROKER = os.environ.get("NOVA_CAN_MQTT_BROKER", "localhost")
-MQTT_PORT = int(os.environ.get("NOVA_CAN_MQTT_PORT", 1883))
+MQTT_PORT = int(os.environ.get("NOVA_CAN_MQTT_PORT", 8883))
 MQTT_TOPIC_PREFIX = os.environ.get("NOVA_CAN_MQTT_TOPIC_PREFIX", "rover")
+MQTT_USERNAME = os.environ.get("NOVA_CAN_MQTT_USERNAME", "nova")
+MQTT_PASSWORD = os.environ.get("NOVA_CAN_MQTT_PASSWORD", "rovanova")
 
 # Set the environment variable
 # Required for the system to be composed correctly, change to the path where your systems are located
 if 'NOVA_CAN_SYSTEMS_PATH' not in os.environ:   
-    os.environ['NOVA_CAN_SYSTEMS_PATH'] = '/home/pih/FYP/nova-can/examples/systems'
+    os.environ['NOVA_CAN_SYSTEMS_PATH'] = '/home/pi/nova-can/examples/systems'
 if 'NOVA_CAN_INTERFACES_PATH' not in os.environ:
-    os.environ['NOVA_CAN_INTERFACES_PATH'] = '/home/pih/FYP/nova-can/examples/interfaces'
+    os.environ['NOVA_CAN_INTERFACES_PATH'] = '/home/pi/nova-can/examples/interfaces'
 
 # no spaces allowed in .yaml files
 # port_type has to be a nova_dsdl type, not nova type
@@ -48,19 +50,19 @@ def get_device_type(system_info, device_name: str) -> str:
 #export PYTHONPATH=/home/pih/FYP/nova-can/dsdl_python_bindings_dir:$PYTHONPATH
 
 
-def can_to_mqtt_callback_factory(system_info):
+def can_to_mqtt_callback_factory(system_info, client):
     def callback(system_name: str, device_name: str, port: object, data: dict):
         dtype = get_device_type(system_info, device_name)
         print("Device Type:", dtype)
         topic = f"{MQTT_TOPIC_PREFIX}.{system_name}.{dtype}.{device_name}.{port.name}"
         topic = topic.lower()
-        payload = str(data)
-        mqtt_client.publish(topic, payload)
+        payload = f'{{"timestamp": {time.time() * 1000}, "value": {data["value"]}}}'
+        client.publish(topic, payload)
         print(f"Published to MQTT: {topic} -> {payload}")
     return callback
 
-def start_can_receiver(system_info):
-    receiver = CanReceiver(system_info, can_to_mqtt_callback_factory(system_info), receiver_id=0)
+def start_can_receiver(system_info, mqtt_client):
+    receiver = CanReceiver(system_info, can_to_mqtt_callback_factory(system_info, mqtt_client), receiver_id=0)
     receiver.run()
 
 if __name__ == "__main__":
@@ -77,10 +79,23 @@ if __name__ == "__main__":
     for device in system_info.devices.values():
         print(device.device_type)  # dot notation
 
+
+    def on_connect(client, userdata, flags, rc, properties=None):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print(f"Failed to connect, return code {rc}")
+
     # Set up MQTT client
-    mqtt_client = mqtt.Client()
-    mqtt_client.connect(MQTT_BROKER, MQTT_PORT, 60)
-    mqtt_client.loop_start()
+    client = mqtt_client.Client(
+        client_id=f'{random.randint(0, 1000)}',
+        transport='websockets',
+        callback_api_version=CallbackAPIVersion.VERSION2
+    )
+    client.username_pw_set(MQTT_USERNAME, MQTT_PASSWORD)
+    client.on_connect = on_connect
+    client.connect(MQTT_BROKER, MQTT_PORT)
+    client.loop_start()
 
 
     # Start CAN receiver in a separate thread - may need to thread in the future
@@ -88,4 +103,4 @@ if __name__ == "__main__":
     # can_thread = threading.Thread(target=start_can_receiver, args=(system_info,), daemon=True)
     # can_thread.start()
 
-    start_can_receiver(system_info)
+    start_can_receiver(system_info, client)
