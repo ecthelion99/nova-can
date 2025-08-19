33cdd8115bf22465e9735b6d0b644ac42794597c
Harrison <hrya0004@student.monash.edu>
2025-08-19 17:26:31 +1000

    updated description

--- src/python/tooling/mqtt_handler/can_mqtt_handler.py
+++ src/python/tooling/mqtt_handler/can_mqtt_handler.py
@@ -110,7 +110,7 @@ def start_gateway(
 # ---------- Command-Line Interface ----------
 def start_gateway_cli():
     parser = argparse.ArgumentParser(
-        description="Starts a CAN to MQTT gateway that listens for CAN messages and publishes them to an MQTT broker."
+        description="Starts a CAN to MQTT gateway that listens for CAN messages and publishes them to an MQTT broker.\n The file path to the system info (.yaml files) needs to be provided via environment variables.\n NOVA_CAN_INTERFACES_PATH and NOVA_CAN_SYSTEMS_PATH"
     )
     parser.add_argument("-b", "--broker", type=str, default=DEFAULT_MQTT_BROKER, help="MQTT broker hostname")
     parser.add_argument("-p", "--port", type=int, default=DEFAULT_MQTT_PORT, help="MQTT broker port")
