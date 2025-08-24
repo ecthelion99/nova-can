5946053d0d7f79c1b883593d17b71fabeff6e536
anthony-volta <66508069+anthony-volta@users.noreply.github.com>
2025-08-24 23:13:34 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setup(
             "ncc = ncc.ncc:main",
             "compose_report = nova_can.utils.compose_system:compose_report",
             "mqtt_can_handler = tooling.mqtt_handler.can_mqtt_handler:start_gateway_cli",
+            "db_can_handler = tooling.db_handler.can_db_handler:start_gateway_cli"
         ]
     },
 )
