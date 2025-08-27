7e4f84b89d63b7266a4f550be9a87c3cf03c1453
Harrison Ryan <harrison.ryan0309@gmail.com>
2025-08-27 18:05:46 +1000

    modified example to align with firmware on BLCMD

--- examples/shell/msg_send_command.sh
+++ examples/shell/msg_send_command.sh
@@ -8,28 +8,25 @@ fi
 
 # Check if correct number of arguments
 if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <destination_node_id> <mode> <value>"
-    echo "Modes:"
-    echo "  0 - CURRENT"
-    echo "  1 - VELOCITY"
-    echo "  2 - POSITION"
+    echo "Usage: $0 <destination_node_id> <port_id> <value>"
+    echo "port_id's:"
+    echo "  33 - CURRENT"
+    echo "  34 - VELOCITY"
+    echo "  35 - POSITION"
     exit 1
 fi
 
 DEST_NODE_ID=$1
-MODE=$2
+PORT_ID=$2
 VALUE=$3
-
-# Port ID for motor driver command message
-PORT_ID=39
 
 # Create CAN ID (assuming standard format)
 # Format: priority(3) | service(1) | service_request(1) | port_id(9) | dest_id(6) | source_id(6)
 CAN_ID=$(( (0 << 26 ) | (0 << 25 ) | (0 << 24 ) | (PORT_ID << 14 ) | (DEST_NODE_ID << 7 ) | NODE_ID ))
 
-FIRST_BYTE=$(($MODE | (($VALUE & 0x3F) << 2)))
-SECOND_BYTE=$((($VALUE >> 6) & 0xFF))
-THIRD_BYTE=$(($VALUE >> 14))
+FIRST_BYTE=0x00
+SECOND_BYTE=$(($VALUE & 0xFF))
+THIRD_BYTE=$((($VALUE >> 8) & 0xFF))
 DATA=$(printf "%02x%02x%02x" $FIRST_BYTE $SECOND_BYTE $THIRD_BYTE)
 
 
